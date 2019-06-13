71f4116d3cc59d0e46a81564386f758551b00075
mgechev <mgechev@gmail.com>
2019-06-13 12:32:55 -0700

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from '../home/home.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+@NgModule({
+  declarations: [AppComponent, HomeComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'nyan',
+        loadChildren: () => import('./nyan/nyan.module').then(m => m.NyanModule)
+      }
+    ]),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
