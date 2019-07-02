629757a7834f7b426075d73d7758d9dd2091f3ea
mgechev <mgechev@gmail.com>
2019-07-02 15:13:10 -0700

    Use github image

--- src/app/nyan/nyan.component.ts
+++ src/app/nyan/nyan.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nyan',
-  template: '<img src="/assets/nyan.png">',
+  template:
+    '<img src="https://github.com/mgechev/service-worker-web-dev/blob/master/src/assets/nyan.png?raw=true">',
   styleUrls: ['./nyan.component.css']
 })
 export class NyanComponent {}
